7222d956fbfdf63113a067b5792142276041566a
tuchao-github <1175608988@qq.com>
2021-05-10 23:37:36 +0800

    修改注解

--- src/main/java/com/lagou/edu/annotation/MyAutowired.java
+++ src/main/java/com/lagou/edu/annotation/MyAutowired.java
@@ -4,9 +4,8 @@ import java.lang.annotation.*;
  *自定义Autowired注解
  * @author Administrator
  */
-@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-@Documented
 public @interface MyAutowired {
     boolean value() default true;
 }
--- src/main/java/com/lagou/edu/annotation/MyTransactional.java
+++ src/main/java/com/lagou/edu/annotation/MyTransactional.java
@@ -6,7 +6,6 @@ import java.lang.annotation.*;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Documented
 public @interface MyTransactional {
     String value() default "TransactionManager";
 }
